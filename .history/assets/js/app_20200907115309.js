4bc511a3f1d28eee314cf41e540e763a020b8cf4
Elfebleu9 <poetic.jm@orange.fr>
2020-09-12 11:38:23 +0200

    gros commit

---
+++ .history/assets/js/app_20200907115309.js
@@ -0,0 +1,196 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import $ from 'jquery';
+// import '../sons';
+import 'bootstrap';
+import '../scss/app.scss';
+import '../sons';
+
+
+
+
+
+// Barres "color" sous les onglets
+let change1 = document.getElementById('b1');
+let bar1 = document.getElementById('a1');
+change1.addEventListener('mouseover',changeback1);
+change1.addEventListener('mouseout',resetback1);
+
+function changeback1(){
+    bar1.style.backgroundColor = 'rgb(246, 246, 18)';
+}
+ function resetback1(){
+    bar1.style.backgroundColor ='';
+}
+
+let change2 = document.getElementById('b2');
+let bar2 = document.getElementById('a2');
+change2.addEventListener('mouseover',changeback2);
+change2.addEventListener('mouseout',resetback2);
+
+function changeback2(){
+    bar2.style.backgroundColor = 'rgb(61, 41, 243)';
+}
+ function resetback2(){
+    bar2.style.backgroundColor ='';
+}
+
+let change3 = document.getElementById('b3');
+let bar3 = document.getElementById('a3');
+change3.addEventListener('mouseover',changeback3);
+change3.addEventListener('mouseout',resetback3);
+
+function changeback3(){
+    bar3.style.backgroundColor = 'rgb(12, 173, 25)';
+}
+ function resetback3(){
+    bar3.style.backgroundColor ='';
+}
+
+let change4 = document.getElementById('b4');
+let bar4 = document.getElementById('a4');
+change4.addEventListener('mouseover',changeback4);
+change4.addEventListener('mouseout',resetback4);
+
+function changeback4(){
+    bar4.style.backgroundColor = 'rgb(241, 136, 16)';
+}
+ function resetback4(){
+    bar4.style.backgroundColor ='';
+}
+
+//déplacement-arrêt barre menu
+
+let navh = document.querySelector('header');
+let navh2 = navh.offsetTop;
+
+window.onscroll= function(){
+    if(window.pageYOffset >navh2){
+        navh.style.position ='fixed';
+        navh.style.top=0;
+        navh.style.width="100%";
+        
+    }
+    else{
+        navh.style.position='static';
+        
+    }
+} 
+
+// synchro barre controls - img
+
+let change5 = document.querySelector('.img-thumbnail');
+let barre = document.querySelector('.controls');
+change5.addEventListener('mouseover',changeback5);
+change5.addEventListener('mouseout',resetback5);
+
+function changeback5(){
+    barre.style.transition = 'transform 0.3s ease-out';
+    barre.style.transform = 'scale(1.075)';
+}
+ function resetback5(){
+    barre.style.transition = 'transform 0.3s ease-out';
+    barre.style.transform = 'scale(1)';
+}
+
+
+
+//lecteur audio
+
+
+
+let audio = document.querySelectorAll('.audio');
+// let juice = document.querySelectorAll('.orange-juice');
+// let btn = document.getElementById('play-pause');
+let volumeslider = document.getElementById('volumeSlider');
+// let orangeBar = document.querySelectorAll('.orange-bar');
+
+// $('#audiopl').html('<audio src="{{extrait.morceau}}" crossOrigin="anonymous" preload="auto" type="audio/mp3"></audio>');
+
+
+
+
+//mise en route lecture
+
+
+
+function togglePlayPause() {
+
+   
+
+    audio .addEventListener("play", function(){
+        if(audio.paused){
+            btn.className="pause";
+            audio.play();
+        }
+        else {
+            btn.className = "play";
+            audio.pause();
+        }
+    })
+}
+ console.log(audio);
+
+// btn.onclick = function(){
+//     togglePlayPause();
+// }
+
+// barre orange
+
+// audio.addEventListener("timeupdate", function(){
+
+//     let juicePos = audio.currentTime / audio.duration;
+
+//     juice.style.width = juicePos * 100 + '%';
+
+//     if(audio.ended) {
+//         btn.className ="play";
+//     }
+
+
+// })
+
+// Volume
+
+
+volumeslider.addEventListener('change', function(){
+
+
+    audio.volume = volumeslider.value / 100;
+
+})
+
+
+// la barre orange clikable
+
+// let rect = orangeBar.getBoundingClientRect();
+// console.log(rect);
+
+// let largeur = rect.width;
+
+// orangeBar.addEventListener('click', function(e){
+
+
+//     // la valeur de notre click sur x par rapport a notre element
+
+//     let x = e.clientX - rect.left;
+
+//     let widthPercent = ((x*100)/largeur);
+
+//     let currentTimeTrue = (widthPercent * 20 ) / 100;
+
+//     // position en secondes
+//     audio1.currentTime = currentTimeTrue;
+//     // barre orange qui va la ou on clique
+//     juice.style.width = widthPercent + '%';
+
+// }) 
+
+
+
---
+++ .history/webpack.config_20200906114559.js
@@ -0,0 +1,110 @@
+var Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    /*
+     * ENTRY CONFIG
+     *
+     * Add 1 entry for each "page" of your app
+     * (including one that's included on every page - e.g. "app")
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('app', './assets/js/app.js')
+    .addEntry('app2', './assets/js/app2.js')
+    //.addEntry('page2', './assets/js/page2.js')
+
+    // Alias
+    // .addAliases({
+    //     'App': `${__dirname}/assets/js`,
+    //     'Style': `${__dirname}/assets/scss`,
+    //     'Images': `${__dirname}/assets/image`,
+    //     'Sons': `${__dirname}/assets/sons`,
+    // })
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+    /*
+     * FEATURE CONFIG
+     *
+     * Enable & configure other features below. For a full
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
+    // enables Sass/SCSS support
+    .enableSassLoader()
+
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
+
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    //.enableIntegrityHashes(Encore.isProduction())
+
+    // uncomment if you're having problems with a jQuery plugin
+    //.autoProvidejQuery()
+
+    // uncomment if you use API Platform Admin (composer req api-admin)
+    //.enableReactPreset()
+    //.addEntry('admin', './assets/js/admin.js')
+
+    .copyFiles([{
+        from: './assets/image',
+        to: '[path][name].[hash:8].[ext]',
+        context: './assets'
+    },
+    {
+        from: './assets/sons',
+        to: '[path][name].[hash:8].[ext]',
+        context: './assets'
+    }
+    ])
+    
+;
+module.exports = {
+    module: {
+        rules: [
+        {
+            test: /\.(mp3)$/i,
+            loader: 'file-loader',
+            options: {
+                name: '[path][name].[ext]',
+                outputPath: 'sons',
+                publicPath: 'assets',
+            },
+        },
+        ],
+    },
+};
+
+module.exports = Encore.getWebpackConfig();
